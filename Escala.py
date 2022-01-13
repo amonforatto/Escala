103248208ff2b0e7e53cd29b5cb3a327932bdf06
Amon Foratto <amon_foratto@hotmail.com>
2022-01-12 22:24:49 -0300

    Primeiro comimt do meu projeto para escala de serviço

---
+++ Escala.py
@@ -0,0 +1,15 @@
+import tkinter
+import requests
+import os
+from tkinter import *
+
+
+escala = Tk()
+escala.title ("Escala de Serviço")
+
+canvas = tkinter.Canvas (escala, height=700, width=600, bg= "grey")
+canvas.pack()
+
+
+
+escala.mainloop()
